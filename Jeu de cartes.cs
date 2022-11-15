90ade0784b213e1829f0d2547372472cd210c4ab
Nathan <2131575@etudiant.cegepvicto.ca>
2022-11-15 15:12:27 -0500

    Premier dépot

---
+++ Jeu de cartes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jeu_de_cartes
+{
+    public class Jeu_de_cartes
+    {
+        public int As = 1;
+        public int deux = 2;
+        public int trois = 3;
+        public int quatre = 4;
+        public int cinq = 5;
+        public int six = 6;
+        public int sept = 7;
+        public int huit = 8;
+        public int neuf = 9;
+        public int dix = 10;
+        public int valet = 11;
+        public int dame = 12;
+        public int roi = 13;
+
+        public Jeu_de_cartes()
+        {
+            string pique = "";
+            string carreau = "";
+            string coeur = "";
+            string trefle = "";
+
+
+        }
+    }
+}
